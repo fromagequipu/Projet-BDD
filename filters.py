01446b8c1228f41f261a6f3a7c882f3b6f08a85d
cbrard <camillebrard04@gmail.com>
2026-01-13 11:05:09 +0100

    filter avec exécution

--- filters.py
+++ filters.py
@@ -1,5 +1,7 @@
 # REQUETES DES FILTRES
 
+import sqlite3
+
 def get_conformites(cursor):
     """Returns all the associations from the database.
   
@@ -24,3 +26,25 @@ def get_conformites(cursor):
         return None
     print("Le resultat est", row)
     return row
+
+
+
+# Entry point of this module.
+if __name__ == '__main__':
+    
+    # Chemin de la BDD
+    db_file = "WaterQuality.db"
+
+    # Connects to the database.
+    conn = sqlite3.connect(db_file)
+    
+    # Enables the foreign key contraints support in SQLite.
+    conn.execute("PRAGMA foreign_keys = 1")
+    
+    # Get the cursor for the connection. This object is used to execute queries 
+    # in the database.
+    cursor = conn.cursor()
+
+    # Close the connection to the database.
+    cursor.close()
+    conn.close()
