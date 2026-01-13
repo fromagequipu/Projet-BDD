d841300f4871af64dc06d1592bbf4b61f3447ab1
cbrard <camillebrard04@gmail.com>
2026-01-13 11:15:00 +0100

    debut F2

--- filters.py
+++ filters.py
@@ -2,7 +2,7 @@
 
 import sqlite3
 
-def get_conformites(cursor):
+def get_communes_conformites(cursor):
     """Returns all the associations from the database.
   
     Parameters
@@ -31,6 +31,37 @@ def get_conformites(cursor):
     print("Le resultat est", row)
     return row
 
+# A MODIFIER 
+def get_communes_dateprel(cursor):
+    """Returns all the associations from the database.
+  
+    Parameters
+    ----------
+    cursor : 
+        The object used to query the database.
+  
+    Returns
+    -------
+    A (possibly, empty) list of all the associations in the database. 
+    Each item of the list is a tuple (asso_name, asso_desc).
+    
+    If an error occurs while querying the database, the function returns None.
+    """
+    try:
+        C1="N"
+        C2="N"
+        C3="N"
+        C4="N"
+        # Reqûete qui permet de visualiser les infos des associations
+        cursor.execute("SELECT c.inseecommune, c.nomcommune FROM Commune c JOIN Prelevement p ON p.cdreseau = c.cdreseau WHERE p.plvconformitechimique = ? AND p.plvconformitebacterio = ? AND p.plvconformitereferencebact = ? AND p.plvconformitereferencechim = ?;", (C1,C2,C3,C4))
+        row = cursor.fetchall()
+    except sqlite3.Error as error:
+        print(error)
+        return None
+    print("Le resultat est", row)
+    return row
+
+
 # Entry point of this module.
 if __name__ == '__main__':
     
@@ -48,7 +79,7 @@ if __name__ == '__main__':
     cursor = conn.cursor()
 
     # TEST FONCTION
-    get_conformites(cursor)
+    get_communes_conformites(cursor)
 
     # Close the connection to the database.
     cursor.close()
