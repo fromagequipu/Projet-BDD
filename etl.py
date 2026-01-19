24f394fb85cc6561f74acd6d4daab767063590d7
gatchd <gatien2003.richard@gmail.com>
2026-01-19 17:37:43 +0100

    etl

--- etl.py
+++ etl.py
@@ -1,35 +1,15 @@
-"""The ETL module.
-
-Look at the instructions after the statement if __name__ == "__main__":
-
-* First, we extract the data from the input CSV files into a collection of Pandas dataframes.
-  Each dataframe corresponds to a table in the target relational database.
-* Then, we transform the data in the dataframes.
-* Finally, we load the data into the database.
-
-"""
-import sys
-print(sys.executable)
 import pandas as pd
 import sqlite3
 import os
 import db
 import utils
-
-#The Python module datetime defines the necessary types for manipulating 
-# dates and times.
-# Among these types, we find one called datetime (the same name as the module) 
-# that is used to express a combination of date and time. 
 from datetime import datetime
 
+#Fonction permettant d'avoir le bon format date
 def get_right_date(input_date):
-    
-    
-    # In case we have an empty string we return None
     if pd.isnull(input_date):
         return None
 
-   
     try:
         input_date = datetime.strptime(input_date, "%Y-%m-%d")
     except ValueError:
@@ -39,30 +19,11 @@ def get_right_date(input_date):
 
 
 def extract():
-    """Implementation of the extraction submodule.
-
-    Returns
-    -------
-    dictionary
-        The collection of dataframes containing the data of the input CSV files.
-        You should have as many dataframes as tables in your relational database.
-        Each dataframe corresponds to a table in the relational database.
-        The dictionary contains a set of key-value pairs where
-            * the value is a dataframe. 
-            * the key is the name of the table corresponding to the dataframe  (e.g., "Student", "EmailAddress"...)
-            
-    """
-
-    # This is the dictonary containing the collection of dataframes.
-    # Each item of this dictionary is a key-value pair; the key is the name of a database table;
-    # the value is a Pandas dataframe with the content of the table.
     dataframes = {}
 
     print("Extracting the data from the input CSV files...")
-
-    ################## TODO: COMPLETE THE CODE OF THIS FUNCTION  #####################
     
-    ### Extraction de nos données dans les deux fichiers CSV ###
+    ### Extraction de nos données dans les trois fichiers CSV ###
     commune = pd.read_csv(
     r"data\dis-2024\DIS_COM_UDI_2024.csv",
     delimiter=',',
@@ -94,33 +55,16 @@ def extract():
     dataframes["Prelevement"] = prelevement_df
     dataframes["Parametre"] = parametre_df
     
-    ##################################################################################
-
+    
     # Return the dataframe collection.
     return dataframes
     
 def transform(dataframes):
-    """Implementation of the transformation submodule.
-
-    Parameters
-    ----------
-    dataframes : dictionary
-        This is the dictionary returned by the function load()
-    
-    Returns 
-    -------
-    The input dictionary (after the transformations).
-    """
 
     print("Transforming the data...")
 
-    ################## TODO: COMPLETE THE CODE OF THIS FUNCTION  #####################
 
     ### 1 - SUPPRESSION DES DUPLICAS DANS LES DONNEES ###
-    #dataframes["Commune"] = dataframes["Commune"].drop_duplicates()
-    #dataframes["Prelevement"] = dataframes["Prelevement"].drop_duplicates()
-    #dataframes["Parametre"] = dataframes["Parametre"].drop_duplicates()
-    #dataframes["Reseau"] = dataframes["Reseau"].drop_duplicates()
     dataframes["Reseau"] = dataframes["Reseau"].drop_duplicates(subset=["cdreseau"])
     dataframes["Commune"] = dataframes["Commune"].drop_duplicates(subset=["inseecommune"])
     dataframes["Prelevement"] = dataframes["Prelevement"].drop_duplicates(subset=["referenceprel"])
@@ -140,74 +84,44 @@ def transform(dataframes):
     for name, df in dataframes.items():
         print(f"{name}: {df.shape}")
     
-    ##################################################################################
 
     # Returns the dataframe collection after the transformations.
     return dataframes
 
 def load(dataframes):
-    """Implementation of the load submodule.
-
-    Parameters:
-    ----------
-    dataframes : dictionary
-        The dictionary returned by the function extract()
-    """
-    # Loads the application configuration.
-   
-
-    # Gets the path to the database file.
+    #Nom de notre bdd
     database_file = "WaterQuality.db"
 
-    # You might bump into some errors while debugging your code which 
-    # This might result in a database that is partially filled with some data.
-    # Each time you rerun the ETL module, you want the database to be in the same state as when
-    # you first created. 
-    # The simpler solution here is to remove the database and recreate the tables back again.
     if os.path.exists(database_file):
-        # If the database file already exists, we remove it.
-        # In order to test the existence of a file, and to remove it, we use functions that are 
-        # available in a Python module called "os".
+        #Si un fichier avec le même nom existe, on le supprime
         os.remove(database_file)
     
-    # We open a connection to the database.
+    # Ouverture de la connexion à notre bdd.
     conn = sqlite3.connect(database_file)
 
-    # We get the cursor to query the database.
     cursor = conn.cursor()
 
-    # We create the tables in the database, by using the function create_database that you implemented in the module
-    # db.
+    # Création de notre bdd
     db.create_database(conn, cursor)
 
     print("Loading the data into the database...")
     
-    ################## TODO: COMPLETE THE CODE OF THIS FUNCTION  #####################
     
     # Pour chaque table, on l'importe dans SQL 
     dataframes["Reseau"].to_sql("Reseau", conn, if_exists="append", index=False)
     dataframes["Commune"].to_sql("Commune", conn, if_exists="append", index=False)
     dataframes["Prelevement"].to_sql("Prelevement", conn, if_exists="append", index=False)
     dataframes["Parametre"].to_sql("Parametre", conn, if_exists="append", index=False)
-    
-    ##################################################################################
-    
+        
     print("Done!")
     
-    # We close the connection to the database.
+    # Fermeture de la connexion à la bdd
     cursor.close()
     conn.close()
 
-# Entry point of the ETL module.
 if __name__ == "__main__":
 
-    ################## TODO: COMPLETE THE CODE OF THIS FUNCTION  #####################
-    
-    # REMOVE THIS BEFORE WRITING YOUR CODE.
-    # pass is only added here to avoid the error mark that Visual Studio Code 
-    # uses to indicate some missing code.
     dataframes = extract() # Extraction des données depuis un CSV
     dataframes= transform(dataframes) # Transformation des données
     load(dataframes) # Importation des données dans la BDD
-    ##################################################################################
-    
+   
